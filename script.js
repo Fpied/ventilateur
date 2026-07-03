5282926e06489cba554445ee2a6f36f37155d0a5
Fpied <piedfrancois@laposte.net>
2026-07-03 21:23:45 +0200

    Ajout d'un backend , de nouveau son pour plus de réalisme , et de nouvelle vitesse.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 const sonVentilateur = new Audio("ventilateur.mp3");
+const sonVentilateurFort = new Audio("ventilateurfort.mp3");
+let vitesseVentilateur = 0;
 sonVentilateur.loop = true;
 sonVentilateur.volume = 0.4;
+sonVentilateurFort.loop = true;
+sonVentilateurFort.volume = 0.8; 
 
 let ventilateurAllume = false;
 let angle = 0;
@@ -20,12 +24,25 @@ function dessinerBouton(){
 
     ctx.fillStyle = "white";
     ctx.font = "20px arial";
-    if(ventilateurAllume){
-        ctx.fillText("ON", 45, 52);
+    switch(vitesseVentilateur){
+        case 0:
+            ctx.fillText("OFF", 35, 52);
+            
+            break;
+        case 1:
+            ctx.fillText("ON", 35, 52);
+            
+            break;
+        case 2:
 
-    } else{
-        ctx.fillText("OFF", 45, 52);
+            ctx.fillText("MOYEN", 20, 52);
+            
 
+            break;
+        case 3:
+            ctx.fillText("RAPIDE", 20, 52);
+            
+            break;
     }
     
 }
@@ -43,65 +60,48 @@ canvas.addEventListener("click", function(event){
         sourisY >= button.y &&
         sourisY <= button.y + button.hauteur
     ){
-        ventilateurAllume = !ventilateurAllume;
+        vitesseVentilateur++;
 
-    if (ventilateurAllume) {
-        sonVentilateur.play();
-    } else {
-        sonVentilateur.pause();
-        sonVentilateur.currentTime = 0;
-    }
+        if(vitesseVentilateur > 3){
+            vitesseVentilateur = 0;
+        }
+
+        ventilateurAllume = vitesseVentilateur > 0;
+        changerSon();
+
+    // if (ventilateurAllume) {
+    //     sonVentilateur.play().catch(function(error){
+    //         console.log("Le son ne démarre pas :", error);
+    //     });
+    // } else {
+    //     sonVentilateur.pause();
+    //     sonVentilateur.currentTime = 0;
+    // }
 
     dessinerBouton();
     }
 
 });
 
-function dessinerCentreVentilateur(){
-    ctx.beginPath();
-    ctx.arc(400, 300, 25, 0, Math.PI *2);
-    ctx.fillStyle = "gray";
-    ctx.fill();
 
-}
-
-dessinerCentreVentilateur();
-
-function dessinerPale(){
-    ctx.beginPath();
-    ctx.ellipse(400, 220, 20, 70, 0, 0, Math.PI *2);
-    ctx.fillStyle = "lightgray";
-    ctx.fill();
-    ctx.beginPath();
-    ctx.ellipse(400, 380, 20, 70, 0, 0, Math.PI * 2);
-    ctx.fillStyle = "lightgray";
-    ctx.fill();
-    ctx.beginPath();
-    ctx.ellipse(320, 300, 70, 20, 0, 0, Math.PI * 2);
-    ctx.fillStyle = "lightgray";
-    ctx.fill();
-    ctx.beginPath();
-    ctx.ellipse(480, 300, 70, 20, 0, 0, Math.PI * 2);
-    ctx.fillStyle = "lightgray";
-    ctx.fill();
-}
-
-dessinerPale();
 
 function dessinerVentillateur(){
     ctx.save();
     ctx.translate(400, 300);
     ctx.rotate(angle);
-    for(let i = 0; i < 4; i++){
+    for(let i = 0; i < 3; i++){
         ctx.beginPath();
-        ctx.ellipse(0, -80, 20, 70, 0, 0, Math.PI *2);
+        ctx.ellipse(0, -120, 50, 150, 0, 0, Math.PI *2);
         ctx.fillStyle = "lightgray";
         ctx.fill();
-        ctx.rotate(Math.PI / 2);
+        ctx.rotate(Math.PI * 2/3);
         
 
     }
+    ctx.beginPath();
     ctx.arc(0, 0, 25, 0, Math.PI * 2);
+    ctx.fillStyle = "gray";
+    ctx.fill();
     ctx.restore();
 
     
@@ -109,17 +109,59 @@ function dessinerVentillateur(){
 }
 dessinerVentillateur();
 
+function dessinerFond(){
+    ctx.fillStyle= "#dbeade";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function dessinerPied(){
+    // tige du pied
+    ctx.fillStyle = "gray";
+    ctx.fillRect(390, 330, 20, 180);
+
+    // socle
+    ctx.fillStyle = "darkgray";
+    ctx.beginPath();
+    ctx.ellipse(400, 530, 90, 25, 0, 0, Math.PI * 2);
+    ctx.fill();
+}
+
 function animation(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    dessinerFond();
+    dessinerPied();
     dessinerBouton();
     dessinerVentillateur();
     requestAnimationFrame(animation);
     if(ventilateurAllume){
-        angle += 0.02;
+        angle += vitesseVentilateur * 0.08;
 
     }
 }
 
 animation();
 
+sonVentilateur.addEventListener("error", function(){
+    console.log("Erreur chargement son");
+});
+
+function changerSon(){
+    sonVentilateur.pause();
+    sonVentilateur.currentTime = 0;
+
+    if(vitesseVentilateur === 0){
+        return;
+    }
+
+    if(vitesseVentilateur === 1){
+        sonVentilateur.src = "ventilateur.mp3";
+    } else if(vitesseVentilateur === 2){
+        sonVentilateur.src = "ventilateurfort.mp3";
+    } else if(vitesseVentilateur === 3){
+        sonVentilateur.src = "ventilateurfort.mp3";
+    }
+
+    sonVentilateur.loop = true;
+    sonVentilateur.play();
+}
 
